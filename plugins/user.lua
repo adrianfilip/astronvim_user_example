fd981bb053e3c03a627730081251f4c4db511006
Adrian Filip <realadrianfilip@gmail.com>
2023-06-10 01:17:00 +0300

    removing copilot because I will add it in community.lua

--- plugins/user.lua
+++ plugins/user.lua
@@ -69,57 +69,6 @@ return {
       require("lsp_lines").setup()
     end,
   },
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    config = function()
-      require("copilot").setup(
-        {
-          panel = {
-            enabled = true,
-            auto_refresh = false,
-            keymap = {
-              jump_prev = "[[",
-              jump_next = "]]",
-              accept = "<CR>",
-              refresh = "gr",
-              open = "<M-CR>"
-            },
-            layout = {
-              position = "bottom", -- | top | left | right
-              ratio = 0.4
-            },
-          },
-          suggestion = {
-            enabled = true,
-            auto_trigger = true,
-            debounce = 75,
-            keymap = {
-              accept = "<M-l>",
-              accept_word = false,
-              accept_line = false,
-              next = "<M-t>",
-              prev = "<M-h>",
-              dismiss = "<C-]>",
-            },
-          },
-          filetypes = {
-            yaml = false,
-            markdown = false,
-            help = false,
-            gitcommit = false,
-            gitrebase = false,
-            hgcommit = false,
-            svn = false,
-            cvs = false,
-            ["."] = false,
-          },
-          copilot_node_command = 'node', -- Node.js version must be > 16.x
-          server_opts_overrides = {},
-        }
-      )
-    end,
-  },
+  -- TODO - is this still needed?
   { 'kevinhwang91/nvim-bqf', ft = 'qf' }
 }
